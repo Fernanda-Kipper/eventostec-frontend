326af296927730b4fc623950f51b1e998516c438
Fernanda <nanda.kipper@gmail.com>
2024-06-02 21:11:24 -0300

    refactor: extract app home to a component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterEventComponent } from './pages/register-event/register-event.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'registra-evento',
+    component: RegisterEventComponent,
+  },
+];
