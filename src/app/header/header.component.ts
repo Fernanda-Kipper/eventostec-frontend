002221badbd93572068e5b6f9654d45c2fb6f471
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-06-03 23:08:41 -0300

    feat: add router link

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
