298e07ac5d6a294805dc032631fec1135308b410
Gabriel Menezes <gabrielmnzs7@gmail.com>
2024-06-07 19:14:33 -0300

    feat: add searchbar on header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  searchTerm: string = '';
+  debounceTimeout: ReturnType<typeof setTimeout> | undefined;
 
+  @Output() searchTermChange: EventEmitter<string> = new EventEmitter<string>();
+  @ViewChild('searchbar') searchInput!: ElementRef;
+
+  onSearchTerm(event: KeyboardEvent) {
+    clearTimeout(this.debounceTimeout);
+    const target = event.target as HTMLInputElement;
+
+    this.debounceTimeout = setTimeout(() => {
+      this.searchTerm = target.value;
+      this.searchTermChange.emit(this.searchTerm);
+    }, 1000);
+  }
 }
