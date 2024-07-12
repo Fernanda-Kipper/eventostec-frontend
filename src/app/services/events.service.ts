2374d219e3f2aa0646ba662f5dfd7cbbedb51848
Fernanda <nanda.kipper@gmail.com>
2024-07-12 17:32:36 -0300

    fix: build configurations

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { Observable, catchError, of } from 'rxjs';
 import { EventItem } from '../types/Event.type';
 
